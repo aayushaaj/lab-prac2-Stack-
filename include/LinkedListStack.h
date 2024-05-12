c43d27d9319cda5cbeb67f05c6b26e0f6d5aaa97
aayusha <aayushajaspau@gmail.com>
2024-05-12 10:13:37 +0545

    corrected destructor declaration

--- include/LinkedListStack.h
+++ include/LinkedListStack.h
@@ -2,16 +2,18 @@
 #include "stack.h"
 #include "../../lab-prac/include/linkedlist.h"
 
-class LinkedListStack:public Stack
+class LinkedListStack: public Stack
 {
 public:
-    LinkedListStack(){}
-    ~LinkedListStack(){}
+    LinkedListStack();
+    ~LinkedListStack();
 
     void push(int item);
     int pop();
     int top();
     bool isEmpty();
+
+    void printStack();
     
  private :
     linkedlist list;
