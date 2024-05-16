8c045c81d9357f5b23d072e7352b2a0f1d6fb34c
aayusha <aayushaaaa.i@gmail.com>
2024-05-16 11:18:50 +0545

    function definitions

--- src/ArrayStack.cpp
+++ src/ArrayStack.cpp
@@ -1,2 +1,43 @@
 #include <iostream>
-#include "..\include\stack.h"
+#include "../include/stack.h"
+#include "../include/ArrayStack.h"
+#include <iostream>
+
+    ArrayStack::ArrayStack() {
+        index = -1;
+    }
+
+    void ArrayStack::push(int item) {
+        if (index >= MAX_SIZE - 1) {
+            std::cout << "Stack is full" << std::endl;
+            return;
+        }
+        arr[++index] = item;
+    }
+
+    int ArrayStack::pop() {
+        if (isEmpty()) {
+            std::cout << "Stack is empty" << std::endl;
+            return -1;
+        }
+        return arr[index--];
+    }
+
+    bool ArrayStack::isEmpty() {
+        return (index < 0);
+    }
+
+    int ArrayStack::top() {
+        if (isEmpty()) {
+            std::cout << "Stack is empty" << std::endl;
+            return -1;
+        }
+        return arr[index];
+    }
+
+    void ArrayStack::printStack() {
+        for (int i = 0; i <= index; i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
