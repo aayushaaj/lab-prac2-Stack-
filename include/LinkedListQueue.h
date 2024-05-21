a8fc0f529238f9aa7dd829320963577163810604
aayusha <aayushaaaa.i@gmail.com>
2024-05-21 06:57:03 +0545

    rear() fn added

--- include/LinkedListQueue.h
+++ include/LinkedListQueue.h
@@ -1,16 +1,21 @@
 #include "Queue.h"
 #include "../../lab-prac/include/linkedlist.h"
+
 class LinkedListQueue : public Queue
 {
     public:
         LinkedListQueue();
         ~LinkedListQueue();
+
         void enqueue(int item);
         int dequeue();
+
         int front();
+        int rear();
         bool isEmpty();
         bool isFull();
         void printQueue();
+
     private:
         linkedlist list;
         int frontElement;
